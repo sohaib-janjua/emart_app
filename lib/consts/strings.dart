e022febf18ef7d91f63cea11d39af9b291e7b6b2
github_username <mastermind.sohaib786@gmail.com>
2022-12-11 23:34:22 +0500

    second commit

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,33 @@
 const appname = "eMart";
 const appversion = "Version 1.0.0";
-const credits = "@Baaba Devs";
+const credits = "@Sohaib Janjua";
+const email = "Email";
+const emailHint = "admin@admin.com";
+const password = "Password";
+const retypePassword = "Retype Password";
+const name = "Name";
+const nameHint = "Sohaib Janjua";
+const passwordHint = "******";
+const forgetPass = "Forget Password";
+const login = "Log in";
+const signup = "Sign up";
+const createNewAccount = "or, create a new account?";
+const loginWith = "Log in with";
+const privacyPolicy = "Privacy Policy";
+const termAndCond = "Terms and Conditions";
+const alreadyHaveAccount = "Already have an account? ";
+
+//home strings
+const home = "Home",
+    categories = "Categories",
+    cart = "Cart",
+    account = "Account";
+
+//home screen strings
+const searchAnyThing = "Search Anything...",
+    todayDeal = "Today Deal",
+    flashSale = "Flash Sale",
+    topSellers = "Top Sellers",
+    brand = "Brand",
+    topCategories = "Top Categories",
+    featuredCategories = "Featured Categories";
